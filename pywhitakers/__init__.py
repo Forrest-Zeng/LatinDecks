7260890ac458dc805cf33a54cc52b7656bd98096
Alrond <ForrestZengMusic@gmail.com>
2024-08-12 18:38:10 -0700

    removed colon

--- pywhitakers/__init__.py
+++ pywhitakers/__init__.py
@@ -39,7 +39,7 @@ class Translator():
         time.sleep(delay)
 
         headers = {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
-                   'Connection:':"keep-alive",
+                   'Connection':'keep-alive',
                   'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
 'Accept':'*/*',
 'Accept-Language':'en-US,en'}
